86e671d36d42416d5b659bcad838bd401c7a73e0
Sesugh Agbadu <sesughagbadu@yahoo.com>
2024-07-06 16:20:11 +0100

    fix pdf react

--- next.config.mjs
+++ next.config.mjs
@@ -19,22 +19,22 @@ const nextConfig = {
         AWS_REGION: process.env.NEXT_PUBLIC_AWS_REGION,
         AWS_S3_BUCKET_NAME: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
     },
-    webpack: (config) => {
-        const pdfWorkerPath = `./node_modules/pdfjs-dist/build/pdf.worker.min.mjs`;
+    // webpack: (config) => {
+    //     const pdfWorkerPath = `./node_modules/pdfjs-dist/build/pdf.worker.min.mjs`;
 
-        config.plugins.push(
-            new CopyPlugin({
-                patterns: [
-                    {
-                        from: pdfWorkerPath,
-                        to: path.resolve(process.cwd(), 'public'),
-                    },
-                ],
-            })
-        );
+    //     config.plugins.push(
+    //         new CopyPlugin({
+    //             patterns: [
+    //                 {
+    //                     from: pdfWorkerPath,
+    //                     to: path.resolve(process.cwd(), 'public'),
+    //                 },
+    //             ],
+    //         })
+    //     );
     
-        return config;
-    },
+    //     return config;
+    // },
 };
 
 export default nextConfig;
