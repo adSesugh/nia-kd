9edb84fd88539ce00a5d3e0c8aa2c4035aaa425c
Sesugh Agbadu <sesughagbadu@yahoo.com>
2024-10-11 10:34:38 +0100

    remove pca03.pdf

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -3,9 +3,15 @@ import React from 'react'
 
 const Gallery = () => {
     return (
-        <div>
+        <div className='bg-gray-400 w-full h-full overflow-y-scroll'>
             <SubHeader title='Gallery' subtitle={'Explore our latest galleries'} />
-            <div>Gallery</div>
+            <div className='px-32 py-12 w-full'>
+                {/* Proposed NIAKD secretariat  */}
+                <div>
+                    <h1 className='text-white my-2'>Proposed NIAKD Secretariat.</h1>
+                    <div className='grid grid-cols-2 place-content-center gap-3'></div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/landing/team/index.tsx
+++ src/components/landing/team/index.tsx
@@ -1,45 +1,7 @@
 import React from 'react'
 import styles from '@/styles/home.module.css'
 import TeamCard from './TeamCard'
-
-const members = [
-    {
-        imageUrl: '/assets/team/9.png',
-        name: 'Arc. Stephen. J. Filiya FNIA',
-        designation: 'Chairman, NIA Kaduna',
-        social_media: {
-            twitter: '',
-            linkedin: ''
-        }
-    },
-    {
-        imageUrl: '/assets/team/10.png',
-        name: 'Arc. Binta Danmaliki FNIA',
-        designation: 'Vice Chairman',
-        social_media: {
-            twitter: '',
-            linkedin: ''
-        }
-    },
-    {
-        imageUrl: '/assets/team/2.png',
-        name: 'Arc. Danjuma S. Ageni MNIA',
-        designation: 'General Secretary',
-        social_media: {
-            twitter: 'https://x.com/DanjAgeni',
-            linkedin: 'https://www.linkedin.com/in/danjuma-ageni-mpm-mnia-173b7279'
-        }
-    },
-    {
-        imageUrl: '/assets/team/4.png',
-        name: 'Arc. Gbenga Popoola MNIA',
-        designation: 'Treasurer',
-        social_media: {
-            twitter: '',
-            linkedin: ''
-        }
-    }
-]
+import members from '@/assets/data/members.json'
 
 const NIATeam = () => {
     return (
@@ -48,7 +10,7 @@ const NIATeam = () => {
                 <h1>Meet Our Team</h1>
             </div>
             <div className={styles.teamMember}>
-                {members.map((member, index) => (
+                {members.slice(0, 4).map((member, index) => (
                     <TeamCard {...member} key={index} />
                 ))}
             </div>
